2fa91c66ffa82bc3a65a49a6fca09a7dbbc56afb
luke.inger@jcb-finance.co.uk <luke.inger@jcb-finance.co.uk>
2021-02-22 19:18:43 +0000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Luke Inger on 22/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
